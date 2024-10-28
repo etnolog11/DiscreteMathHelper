0bdfe2e141bc9280ef3b45e4b3c77c6e5b7642fc
etnolog11 <volkovvladyslav.off@gmail.com>
2024-10-28 18:48:59 +0100

    two config files have changed implemented a function to bring a new parameter in an expression, however the fusion of two truthtables is stil not implemented.

--- src/Operations.java
+++ src/Operations.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.function.BiPredicate;
 
 class Operations {
@@ -8,6 +9,33 @@ class Operations {
     static {
         shortNames.put("and",(BiPredicate<Boolean, Boolean>) (aBoolean, aBoolean2) ->  aBoolean && aBoolean2);
 
+    }
+    //TODO check if all of the key counts have INT and not SHORT
+    private static TruthTable addMissingParameters(TruthTable oldTable, List<boolean[]> newKeys,List<Parameter> newParams){
+        HashMap<Integer,Integer> transformIndexFromNewToOld = new HashMap<>();
+        HashMap<Integer,Integer> transformIndexFromOldToNew = new HashMap<>();
+        List<Parameter> oldParams = oldTable.getParameters();
+        //create transform dictionary
+        for (int i = 0; i < oldParams.size() ; i++) {
+            for(int j = 0; j < newParams.size();j++){
+                if (oldParams.get(i).equals(newParams.get(j))){
+                    transformIndexFromNewToOld.put(j,i);
+                    transformIndexFromOldToNew.put(i,j);
+                }
+            }
+        }
+        /*
+        for every final key find the corresponding original key and get the value to that key
+        */
+        HashMap<boolean[],Boolean> newValues = new HashMap<boolean[],Boolean>();
+        boolean[] oldkey= new boolean[oldParams.size()];
+        for (boolean[] key:newKeys) {
+            for (int i = 0; i < oldParams.size(); i++) {
+                oldkey[i]= key [transformIndexFromOldToNew.get(i)];
+            }
+            newValues.put(key,oldTable.getValues().get(oldkey));
+        }
+        return new TruthTable(newParams,newValues);
     }
     public static Statement process(Statement first,Statement second, String operation){
         TruthTable originalFirst = first.getTruthTable();
@@ -38,9 +66,18 @@ class Operations {
         else {
             newKeys= new ArrayList<>(first.getTruthTable().getValues().keySet());
         }
+
+        if (newParams.size()!=originalFirst.getParameters().size()){
+            originalFirst=addMissingParameters(originalFirst, newKeys,newParams);
+        }
+        if(newParams.size()!=originalSecond.getParameters().size()){
+            originalSecond=addMissingParameters(originalSecond, newKeys,newParams);
+        }
         HashMap<boolean[],Boolean> newAnswers =new HashMap<boolean[], Boolean>();
+        /*TODO fuse to tables. First make a map of keys with parameter in different orders and then apply BiPredicate on a key and mapped key to get the final value
+         make sure the order matches with new keys and parameters*/
         for (boolean[] keys:newKeys){
-            
+
         }
         TruthTable newTruthTable= new TruthTable(newParams,newAnswers);
         return new Statement("sffd");
